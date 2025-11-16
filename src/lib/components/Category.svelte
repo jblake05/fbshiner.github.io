612740784c6646b7140a3915ee28c15d5eda2b3b
Jeff Blake <jtblake@middlebury.edu>
2025-11-16 16:42:58 -0600

    Category component

---
+++ src/lib/components/Category.svelte
@@ -0,0 +1,18 @@
+<script>
+    let {data, cwd} = $props();
+
+    import Grid from "$lib/components/Grid.svelte";
+</script>
+
+<style>
+.inner {
+    width: 100%;
+}
+</style>
+
+<div class="inner" style={`rgb(${r(0, 255)}, ${r(0, 255)}, ${r(0, 255)});`}>
+<h1>{cwd}</h1>
+
+<!-- definitely a better way to do this other than cwd as a prop -->
+<Grid data={data} cwd={cwd}></Grid>
+</div>
